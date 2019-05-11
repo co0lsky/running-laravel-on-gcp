3759bc04077b78ee0cece71b2792cd46e46451b4
Sky Chin <sky@avana.asia>
2019-05-11 13:22:31 +0800

    upload image to gsc

---
+++ app/Http/Controllers/UserAvatarController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAvatarController extends Controller
+{
+    /**
+     * Update the avatar for the user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        $path = $request->file('avatar')->store('avatars');
+
+        return $path;
+    }
+}
